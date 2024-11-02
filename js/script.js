8d1881ee1f3456c44af0a13f71a22a8288bc2f4e
youssefassal <153854962+youssefassal@users.noreply.github.com>
2024-11-02 13:21:22 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var btn0 = document.querySelector(".send0")
+var btn1 = document.querySelector(".send1")
+var btn2 = document.querySelector(".send2")
+
+function code1(){
+    alert("Message sent Successfully")
+}
+
+btn0.onclick = code1
+btn1.onclick = code1
+btn2.onclick = code1
